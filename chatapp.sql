6d75755765345de0ac408c3bbf0fdd0bcfd3fd69
Sayan Adhikary <147737664+leoGoD10@users.noreply.github.com>
2025-01-29 11:11:06 +0530

    Update chatapp.sql

--- chatapp.sql
+++ chatapp.sql
@@ -4,7 +4,7 @@ CREATE TABLE `messages` (
   `msg_id` int(11) NOT NULL,
   `incoming_msg_id` int(255) NOT NULL,
   `outgoing_msg_id` int(255) NOT NULL,
-  `msg` varchar(1000) NOT NULL
+  `msg` varchar(1000) NOT NULL,
   `sendImg` VARCHAR(255),
   `sendVideo` VARCHAR(255),
   `Senddocument` VARCHAR(255)
